60611904dbb70d3a312248043e796338a5460438
DevDanyal <aidevdanyal@gmail.com>
2025-12-25 18:03:50 +0500

    Create list.py

---
+++ list.py
@@ -0,0 +1,12 @@
+# mans_tuple = (1, 2, 3, 4, 5)
+# print(mans_tuple)
+
+# copies_set = {'mathcopy', 'sciencecopy', 'urducopy', 'sciencecopy'}
+# print(copies_set)
+
+period_list = [1, 2, 3, 4, 5, 4]
+print(period_list)
+
+# dictionary = print({
+#                "name" :  "danyal",
+#                })
